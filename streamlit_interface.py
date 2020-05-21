eef5eda6b19505229cfd4706cec6395e2e55323e
PELAGIVS <54682204+Aspace-dev@users.noreply.github.com>
2020-05-21 16:15:43 -0500

    Updated dashboard file to include working variant with NewYork.csv

--- streamlit_interface.py
+++ streamlit_interface.py
@@ -2,32 +2,22 @@ import streamlit as st
 import pandas as pd
 import requests
 
+# Plotly imports
 import plotly.plotly as py
 import plotly.graph_objs as go
 
-# Streamlit caching template - to be modified to be compatible with modeling results and "State" table
-import time
+# Will include code for caching here from streamlit template - as seen in previous version/commit
 
-@st.cache(suppress_st_warning=True)
-def expensive_computation(a, b):
-    st.write("Cache miss: expensive_computation(", a, ",", b, ") ran")
-    time.sleep(2)  # This makes the function take 2s to run
-    return a * b
+# Plot Title for New York csv (will incorporate api data when redeployed)
+st.title("COVID 19 USA Data Dashboard - New York Test")
 
-a = 2
-b = 210  # 👈 Changed this
-res = expensive_computation(a, b)
+ny_df = pd.read_csv('NewYork.csv')
 
-st.write("Result:", res)
-
-
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/wind_speed_laurel_nebraska.csv')
-
-# Plotly template - With error bars - "Actual" data points to be added as well
+# Plot Test v1
 upper_bound = go.Scatter(
     name='Upper Bound',
-    x=df['Time'],
-    y=df['10 Min Sampled Avg']+df['10 Min Std Dev'],
+    x=ny_df['ds'],
+    y=ny_df['yhat_upper'],
     mode='lines',
     marker=dict(color="#444"),
     line=dict(width=0),
@@ -36,8 +26,8 @@ upper_bound = go.Scatter(
 
 trace = go.Scatter(
     name='Measurement',
-    x=df['Time'],
-    y=df['10 Min Sampled Avg'],
+    x=ny_df['ds'],
+    y=ny_df['yhat'],
     mode='lines',
     line=dict(color='rgb(31, 119, 180)'),
     fillcolor='rgba(68, 68, 68, 0.3)',
@@ -45,31 +35,28 @@ trace = go.Scatter(
 
 lower_bound = go.Scatter(
     name='Lower Bound',
-    x=df['Time'],
-    y=df['10 Min Sampled Avg']-df['10 Min Std Dev'],
+    x=ny_df['ds'],
+    y=ny_df["yhat_lower"],
     marker=dict(color="#444"),
     line=dict(width=0),
     mode='lines')
 
-# Trace order can be important
-# with continuous error bars
+# Plot layout with continuous error bars
 data = [lower_bound, trace, upper_bound]
 
 layout = go.Layout(
-    yaxis=dict(title='Wind speed (m/s)'),
-    title='Continuous, variable value error bars.<br>Notice the hover text!',
+    yaxis=dict(title='Number of New Cases - By Day'),
+    title='Number of New Cases in New York - With Error Bars',
     showlegend = False)
 
 fig = go.Figure(data=data, layout=layout)
-py.iplot(fig, filename='pandas-continuous-error-bars')
+st.plotly_chart(fig, filename='pandas-continuous-error-bars')
 
-
-st.title("COVID 19 USA Data Dashboard")
-
+# Raw data table with state selector - throwing error (will be addressed next)
 state_list = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia","Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 state_selector = st.selectbox("Select State", state_list)
 
 state_str = state_selector.replace(' ', "%20")
 state_data_df = pd.read_json(f"https://api-pc6dbtrtla-uc.a.run.app/API/us/timeseries/{state_str}/allcounties")
 st.write(state_data_df[["State", "Totals as of Date", "Cases", "Deaths"]])
-    
+
